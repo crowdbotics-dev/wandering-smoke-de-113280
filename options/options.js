4f72c665c8391d049c5562ba9bb156797179b638
Crowdbotics <engineering@crowdbotics.com>
2023-08-31 23:15:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wandering-smoke-de-113280",
+  url: "https://wandering-smoke-de-113280.botics.co",
+  api: "https://wandering-smoke-de-113280.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
